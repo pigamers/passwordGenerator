ac5c4ac5a6eb3be2ca28cbef5e5f819cbf9da934
Mayank Kumar Sharma <pigamers1123@gmail.com>
2024-05-29 17:32:40 +0000

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
 
   return (
     <>
-      
+      <div className="max-w-xl mx-auto">
+        <div className="flex flex-row justify-center items-center">
+          <input type="text" id="small-input" className="block p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-400 text-xs" />
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">copy</button>
+        </div>
+      </div>
     </>
   )
 }
